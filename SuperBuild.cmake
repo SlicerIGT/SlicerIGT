6d412d8f0c1317d9f01e121621d3f46c788e2a5c
Tamas Ungi <ungi@cs.queensu.ca>
2013-02-06 00:27:40 -0500

    Test: Superbuild and packaging working together.
    Signed-off-by: Tamas Ungi <ungi@cs.queensu.ca>

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -21,28 +21,38 @@ OPTION(SLICERIGT_ENABLE_EXPERIMENTAL_MODULES "Enable the building of work-in-pro
 # Project dependencies
 #-----------------------------------------------------------------------------
 
-set(inner_DEPENDENCIES "CollectFiducialsDownload")
-
-set( proj CollectFiducials )
-
-set( ${proj}_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj} )
-set( ${proj}_BINARY_DIR ${${proj}_SOURCE_DIR}-build )
-
-ExternalProject_Add(
-  ${proj}Download
-  GIT_REPOSITORY "https://github.com/SlicerIGT/CollectFiducials.git"
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  TEST_COMMAND ""
-  INSTALL_COMMAND ""
-  SOURCE_DIR ${${proj}_SOURCE_DIR}
-  BINARY_DIR ${${proj}_BINARY_DIR}
+set( inner_DEPENDENCIES "" )
+  
+set( SlicerIGT_Modules
+  CollectFiducials
+  CreateModels
+  UltrasoundSnapshots
+  VolumeResliceDriver
   )
+
+  
+foreach( proj ${SlicerIGT_Modules} )
+  set( inner_DEPENDENCIES ${proj}Download ${inner_DEPENDENCIES} )
+  set( ${proj}_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj} )
+  set( ${proj}_BINARY_DIR ${${proj}_SOURCE_DIR}-build )
+  message( STATUS "Source: "${${proj}_SOURCE_DIR} )
+  ExternalProject_Add(
+    ${proj}Download
+    GIT_REPOSITORY "https://github.com/SlicerIGT/${proj}.git"
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    TEST_COMMAND ""
+    INSTALL_COMMAND ""
+    SOURCE_DIR ${${proj}_SOURCE_DIR}
+    BINARY_DIR ${${proj}_BINARY_DIR}
+    )
+endforeach( proj )
+
 
 #SlicerMacroCheckExternalProjectDependency(inner)
 
 set(proj inner)
-ExternalProject_Add(${proj}
+ExternalProject_Add( ${proj}
   DOWNLOAD_COMMAND ""
   INSTALL_COMMAND ""
   SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
@@ -60,12 +70,17 @@ ExternalProject_Add(${proj}
     -DEXTENSION_SUPERBUILD_BINARY_DIR:PATH=${${EXTENSION_NAME}_BINARY_DIR}
     -DSLICERRT_ENABLE_EXPERIMENTAL_MODULES:BOOL=${SLICERRT_ENABLE_EXPERIMENTAL_MODULES} 
     -DCollectFiducials_SOURCE_DIR:PATH=${CollectFiducials_SOURCE_DIR}
-    -DCollectFiducials_BINARY_DIR:PATH=${CollectFiducials_BINARY_DIR}     
+    -DCollectFiducials_BINARY_DIR:PATH=${CollectFiducials_BINARY_DIR}
+    -DCreateModels_SOURCE_DIR:PATH=${CreateModels_SOURCE_DIR}
+    -DCreateModels_BINARY_DIR:PATH=${CreateModels_BINARY_DIR}      
+    -DUltrasoundSnapshots_SOURCE_DIR:PATH=${UltrasoundSnapshots_SOURCE_DIR}
+    -DUltrasoundSnapshots_BINARY_DIR:PATH=${UltrasoundSnapshots_BINARY_DIR}      
+    -DVolumeResliceDriver_SOURCE_DIR:PATH=${VolumeResliceDriver_SOURCE_DIR}
+    -DVolumeResliceDriver_BINARY_DIR:PATH=${VolumeResliceDriver_BINARY_DIR}      
     # Slicer
     -DSlicer_DIR:PATH=${Slicer_DIR}
     -DMIDAS_PACKAGE_EMAIL:STRING=${MIDAS_PACKAGE_EMAIL}
     -DMIDAS_PACKAGE_API_KEY:STRING=${MIDAS_PACKAGE_API_KEY}    
     ${ep_cmake_args}
-  DEPENDS
-    ${${proj}_DEPENDENCIES}
+  DEPENDS ${${proj}_DEPENDENCIES}
   )
