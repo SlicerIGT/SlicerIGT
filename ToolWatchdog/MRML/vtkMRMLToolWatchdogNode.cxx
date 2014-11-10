815219611b2c5741a6ab279ee5e9d2cf49de8eef
Guevara <jaime.guevara@queensu.ca>
2014-11-09 22:24:19 -0500

    add node

---
+++ ToolWatchdog/MRML/vtkMRMLToolWatchdogNode.cxx
@@ -0,0 +1,172 @@
+
+// ToolWatchdog MRML includes
+#include "vtkMRMLToolWatchdogNode.h"
+
+// Other MRML includes
+#include "vtkMRMLDisplayNode.h"
+#include "vtkMRMLLinearTransformNode.h"
+#include "vtkMRMLModelNode.h"
+#include "vtkMRMLNode.h"
+#include "vtkMRMLTransformNode.h"
+
+// VTK includes
+#include <vtkDoubleArray.h>
+#include <vtkMath.h>
+#include <vtkSmartPointer.h>
+#include <vtkNew.h>
+#include <vtkIntArray.h>
+#include <vtkCommand.h>
+
+// Other includes
+#include <sstream>
+
+// Constants
+static const char* TOOL_ROLE = "ToolWatchdogTransformNode";
+
+
+
+vtkMRMLToolWatchdogNode* vtkMRMLToolWatchdogNode
+::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance( "vtkMRMLToolWatchdogNode" );
+  if( ret )
+    {
+      return ( vtkMRMLToolWatchdogNode* )ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLToolWatchdogNode;
+}
+
+
+
+vtkMRMLToolWatchdogNode
+::vtkMRMLToolWatchdogNode()
+{
+  this->HideFromEditorsOff();
+  this->SetSaveWithScene( true );
+  
+  this->AddNodeReferenceRole( TOOL_ROLE );
+}
+
+
+
+vtkMRMLToolWatchdogNode
+::~vtkMRMLToolWatchdogNode()
+{
+}
+
+
+
+vtkMRMLNode*
+vtkMRMLToolWatchdogNode
+::CreateNodeInstance()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance( "vtkMRMLToolWatchdogNode" );
+  if( ret )
+    {
+      return ( vtkMRMLToolWatchdogNode* )ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMRMLToolWatchdogNode;
+}
+
+
+
+void
+vtkMRMLToolWatchdogNode
+::WriteXML( ostream& of, int nIndent )
+{
+  Superclass::WriteXML(of, nIndent); // This will take care of referenced nodes
+  vtkIndent indent(nIndent);
+}
+
+
+
+void
+vtkMRMLToolWatchdogNode
+::ReadXMLAttributes( const char** atts )
+{
+  Superclass::ReadXMLAttributes(atts); // This will take care of referenced nodes
+
+  // Read all MRML node attributes from two arrays of names and values
+  const char* attName;
+  const char* attValue;
+
+  while (*atts != NULL)
+  {
+    attName  = *(atts++);
+    attValue = *(atts++);
+    
+    if ( ! strcmp( attName, "WarningColorR" ) )
+    {
+      std::stringstream ss;
+      ss << attValue;
+      double r = 0.0;
+      ss >> r;
+    }
+  }
+}
+
+
+
+void
+vtkMRMLToolWatchdogNode
+::Copy( vtkMRMLNode *anode )
+{  
+  Superclass::Copy( anode ); // This will take care of referenced nodes
+  
+  vtkMRMLToolWatchdogNode *node = ( vtkMRMLToolWatchdogNode* ) anode;
+  
+  
+  this->Modified();
+}
+
+
+
+void
+vtkMRMLToolWatchdogNode
+::PrintSelf( ostream& os, vtkIndent indent )
+{
+  vtkMRMLNode::PrintSelf(os,indent); // This will take care of referenced nodes
+
+  os << indent << "ToolTipTransformID: " << this->GetToolTransformNode()->GetID() << std::endl;
+}
+
+
+vtkMRMLLinearTransformNode*
+vtkMRMLToolWatchdogNode
+::GetToolTransformNode()
+{
+  vtkMRMLLinearTransformNode* ltNode = vtkMRMLLinearTransformNode::SafeDownCast( this->GetNodeReference( TOOL_ROLE ) );
+  return ltNode;
+}
+
+
+
+void
+vtkMRMLToolWatchdogNode
+::SetAndObserveToolTransformNodeId( const char* nodeId )
+{
+  vtkNew<vtkIntArray> events;
+  events->InsertNextValue( vtkCommand::ModifiedEvent );
+  events->InsertNextValue( vtkMRMLLinearTransformNode::TransformModifiedEvent );
+  this->SetAndObserveNodeReferenceID( TOOL_ROLE, nodeId, events.GetPointer() );
+}
+
+
+
+void
+vtkMRMLToolWatchdogNode
+::ProcessMRMLEvents( vtkObject *caller, unsigned long event, void *callData )
+{
+  vtkMRMLNode* callerNode = vtkMRMLNode::SafeDownCast( caller );
+  if ( callerNode == NULL ) return;
+
+  const char* ObservedTransformNodeId = this->GetToolTransformNode()->GetID();
+  if ( strcmp( ObservedTransformNodeId, callerNode->GetID() ) == 0 )
+  {
+    this->Modified(); // This will tell the logic to update
+  }
+}
---
+++ ToolWatchdog/MRML/vtkMRMLToolWatchdogNode.h
@@ -0,0 +1,84 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLToolWatchdogNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:28 $
+  Version:   $Revision: 1.6 $
+
+=========================================================================auto=*/
+
+#ifndef __vtkMRMLToolWatchdogNode_h
+#define __vtkMRMLToolWatchdogNode_h
+
+#include <ctime>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+#include "vtkMRMLNode.h"
+#include "vtkMRMLScene.h"
+#include "vtkObject.h"
+#include "vtkObjectBase.h"
+#include "vtkObjectFactory.h"
+
+// ToolWatchdog includes
+#include "vtkSlicerToolWatchdogModuleMRMLExport.h"
+
+class vtkMRMLLinearTransformNode;
+
+
+class
+VTK_SLICER_TOOLWATCHDOG_MODULE_MRML_EXPORT
+vtkMRMLToolWatchdogNode
+: public vtkMRMLNode
+{
+public:
+  
+  vtkTypeMacro( vtkMRMLToolWatchdogNode, vtkMRMLNode );
+  
+  // Standard MRML node methods  
+
+  static vtkMRMLToolWatchdogNode *New();  
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+  virtual const char* GetNodeTagName() { return "ToolWatchdog"; };
+  void PrintSelf( ostream& os, vtkIndent indent );
+  virtual void ReadXMLAttributes( const char** atts );
+  virtual void WriteXML( ostream& of, int indent );
+  virtual void Copy( vtkMRMLNode *node );
+  
+protected:
+
+  // Constructor/desctructor methods
+
+  vtkMRMLToolWatchdogNode();
+  virtual ~vtkMRMLToolWatchdogNode();
+  vtkMRMLToolWatchdogNode ( const vtkMRMLToolWatchdogNode& );
+  void operator=( const vtkMRMLToolWatchdogNode& );
+ 
+  
+public:
+    vtkGetMacro( LastTimeStamp, unsigned long );
+    vtkSetMacro( LastTimeStamp, unsigned long );
+
+  // Tool transform is interpreted as ToolTip-to-RAS. The origin of ToolTip 
+  // coordinate system is the tip of the surgical tool that needs to avoid the
+  // risk area.
+
+  vtkMRMLLinearTransformNode* GetToolTransformNode();
+  void SetAndObserveToolTransformNodeId( const char* nodeId );
+
+  
+  void ProcessMRMLEvents( vtkObject *caller, unsigned long event, void *callData );
+
+private:
+  unsigned long LastTimeStamp;
+
+};  
+
+#endif
