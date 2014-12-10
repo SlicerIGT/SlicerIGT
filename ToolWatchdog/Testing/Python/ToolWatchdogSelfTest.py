088fbd09fc40082cb7408bf16dbd2a654be28fcf
Guevara <jaime.guevara@queensu.ca>
2014-12-09 19:22:51 -0500

    re #102 test script updated

--- ToolWatchdog/Testing/Python/ToolWatchdogSelfTest.py
+++ ToolWatchdog/Testing/Python/ToolWatchdogSelfTest.py
@@ -13,14 +13,14 @@ class ToolWatchdogSelfTest(ScriptedLoadableModule):
     self.parent.title = "ToolWatchdogSelfTest" # TODO make this more human readable by adding spaces
     self.parent.categories = ["Testing.IGT Tests"]
     self.parent.dependencies = []
-    self.parent.contributors = ["Tamas Ungi (Queen's University) Jaime Garcia-Guevara (Queen's University)"] # replace with "Firstname Lastname (Organization)"
+    self.parent.contributors = ["Tamas Ungi (Queen's University) Jaime Garcia-Guevara (Queen's University)"]
     self.parent.helpText = """
     This is a self test for the tool watchdog module.
     """
     self.parent.acknowledgementText = """
     This work was was funded by Cancer Care Ontario and the Ontario Consortium for Adaptive Interventions in
     Radiation Oncology (OCAIRO)
-""" # replace with organization, grant and thanks.
+"""
 
 #
 # ToolWatchdogSelfTestWidget
@@ -32,32 +32,24 @@ class ToolWatchdogSelfTestWidget(ScriptedLoadableModuleWidget):
     ScriptedLoadableModuleWidget.setup(self)
     # Instantiate and connect widgets ...
 
-    #
-    # Reload and Test area
-    #
-    reloadCollapsibleButton = ctk.ctkCollapsibleButton()
-    reloadCollapsibleButton.text = "Reload && Test"
-    self.layout.addWidget(reloadCollapsibleButton)
-    reloadFormLayout = qt.QFormLayout(reloadCollapsibleButton)
 
-    # reload button
-    # (use this during development, but remove it when delivering
-    #  your module to users)
-    self.reloadButton = qt.QPushButton("Reload")
-    self.reloadButton.toolTip = "Reload this module."
-    self.reloadButton.name = "ToolWatchdogSelfTest Reload"
-    reloadFormLayout.addWidget(self.reloadButton)
-    self.reloadButton.connect('clicked()', self.onReload)
-
-    # reload and test button
-    # (use this during development, but remove it when delivering
-    #  your module to users)
-    self.reloadAndTestButton = qt.QPushButton("Reload and Test")
-    self.reloadAndTestButton.toolTip = "Reload this module and then run the self tests."
-    reloadFormLayout.addWidget(self.reloadAndTestButton)
-    self.reloadAndTestButton.connect('clicked()', self.onReloadAndTest)
-
-    
+    self.usePlusServerCheckBox = qt.QCheckBox()
+    self.usePlusServerCheckBox.setText("Use plus server")
+    speedLabel = qt.QLabel()
+    speedLabel.setText("Speed to execute the test")
+    self.speedSpinBox = qt.QSpinBox()
+    self.speedSpinBox
+    self.speedSpinBox.toolTip = "Speed to execute the test."
+    self.speedSpinBox.name = "ToolWatchdogSelfTest Speed"
+    self.speedSpinBox.setValue(3)
+    hLayout =qt.QHBoxLayout()
+    hLayout.addStretch(1)
+    hLayout.addWidget(self.usePlusServerCheckBox)
+    #hLayout.addItem(qt.QSpacerItem(20, 40, qt.QSizePolicy.Minimum, qt.QSizePolicy.Expanding))
+    hLayout.addWidget(speedLabel)
+    hLayout.addWidget(self.speedSpinBox)
+    self.layout.addLayout(hLayout)
+    #self.speedSpinBox.connect('changed()', self.onSpeedChanged)
     #
     # Parameters Area
     #
@@ -96,11 +88,17 @@ class ToolWatchdogSelfTestWidget(ScriptedLoadableModuleWidget):
     """
     globals()[moduleName] = slicer.util.reloadScriptedModule(moduleName)
 
+  def setSpeed(self,moduleName="ToolWatchdogSelfTest"):
+    globals()[moduleName] = slicer.util.reloadScriptedModule(moduleName)
+
+
   def onReloadAndTest(self,moduleName="ToolWatchdogSelfTest"):
     try:
       self.onReload()
       evalString = 'globals()["%s"].%sTest()' % (moduleName, moduleName)
       tester = eval(evalString)
+      tester.setSpeed(self.speedSpinBox.value)
+      tester.setUsePlusServer(self.usePlusServerCheckBox.isChecked())
       tester.runTest()
     except Exception, e:
       import traceback
@@ -111,7 +109,6 @@ class ToolWatchdogSelfTestWidget(ScriptedLoadableModuleWidget):
 #
 # ToolWatchdogSelfTestLogic
 #
-
 class ToolWatchdogSelfTestLogic(ScriptedLoadableModuleLogic):
   """This class should implement all the actual
   computation done by your module.  The interface
@@ -125,11 +122,18 @@ class ToolWatchdogSelfTestLogic(ScriptedLoadableModuleLogic):
     """
     print("Test the algorith logic here, not implemented yet")
 
-
+#
+# ToolWatchdogSelfTestTest
+#
 class ToolWatchdogSelfTestTest(ScriptedLoadableModuleTest):
   """
   This is the test case for your scripted module.
   """
+  def setSpeed(self, speed=3):
+      self.Speed=speed
+
+  def setUsePlusServer(self, usePlusServer=False):
+      self.UsePlusServer=usePlusServer
 
   def setUp(self):
     """ Do whatever is needed to reset the state - typically a scene clear will be enough.
@@ -154,7 +158,7 @@ class ToolWatchdogSelfTestTest(ScriptedLoadableModuleTest):
     your test should break so they know that the feature is needed.
     """
 
-    self.delayDisplay("Starting the test")
+    self.delayDisplay("Starting the test at speed "+ str(self.Speed))
     scene = slicer.mrmlScene
     mainWindow = slicer.util.mainWindow()
 
@@ -164,9 +168,8 @@ class ToolWatchdogSelfTestTest(ScriptedLoadableModuleTest):
     tools=[]
     watchdogToolNodeCombobox = slicer.util.findChildren(widget=watchdogWidget, name='ToolComboBox')[0]  
     watchdogToolAddToolButton = slicer.util.findChildren(widget=watchdogWidget, name='AddToolButton')[0]
-
-    usePlusServer = True
-    if(usePlusServer):
+    #if plus server connected open IGTLink
+    if(self.UsePlusServer):
         openIGTLinkIFWidget = slicer.modules.openigtlinkif.widgetRepresentation()
         mainWindow.moduleSelector().selectModule('OpenIGTLinkIF')
         openIGTLinkIFAddToolButton = slicer.util.findChildren(widget=openIGTLinkIFWidget, name='AddConnectorButton')[0]
@@ -181,7 +184,7 @@ class ToolWatchdogSelfTestTest(ScriptedLoadableModuleTest):
             watchdogToolAddToolButton.click()
             i=i+1
     
-    #add several transform put them on the tool watchdog list
+    #add several transforms put them on the tool watchdog list
     i=0
     while i<3:
         # Create transform node, add it to the scene and set it in the toolComboBox
@@ -193,11 +196,44 @@ class ToolWatchdogSelfTestTest(ScriptedLoadableModuleTest):
         watchdogToolAddToolButton.click()
         i=i+1
 
-    speed=3
-    self.delayDisplay('Wait...',1100/speed)
-    print(  watchdogToolNodeCombobox.nodeTypes )
-    
-    #verify if the tool watchdog incitator is updateing according to the transform update
+    self.delayDisplay('Wait...', 1100/self.Speed)
+    print( watchdogToolNodeCombobox.nodeTypes )
+
+    #Get gui widgets, move two nodes items in the list 
+    watchdogToolToolsTableWidget = slicer.util.findChildren(widget=watchdogWidget, name='ToolsTableWidget')[0]
+    watchdogToolToolsTableWidget.selectRow(3)
+    self.delayDisplay('Table selection up 3rd row...', 2100)
+    watchdogToolUpToolButton = slicer.util.findChildren(widget=watchdogWidget, name='UpToolButton')[0]
+    watchdogToolUpToolButton.click()
+    watchdogToolDownToolButton = slicer.util.findChildren(widget=watchdogWidget, name='DownToolButton')[0]
+    watchdogToolToolsTableWidget.selectRow(0)
+    self.delayDisplay('Table selection down first element...', 2100)
+    watchdogToolDownToolButton.click()
+
+    #if PlusServer used delete two of the nodes and move the one left to the bottom of the list
+    watchdogToolDeleteToolButton = slicer.util.findChildren(widget=watchdogWidget, name='DeleteToolButton')[0]
+    if(self.UsePlusServer):
+        i=0
+        while i<3:
+            watchdogToolToolsTableWidget.selectRow(i*2)
+            watchdogToolDeleteToolButton.click()
+            i=i+1
+        i=0
+        while i<3:
+            watchdogToolToolsTableWidget.selectRow(i)
+            watchdogToolDownToolButton.click()
+            i=i+1
+
+    #put the status labels in a container to check if they update correctly
+    i=0
+    it=[]
+    while i<3:
+        it.append(watchdogToolToolsTableWidget.item(i,3))
+        i=i+1
+    if(self.UsePlusServer):
+        it.append(watchdogToolToolsTableWidget.item(4,3))
+
+    #verify if the tool watchdog indicator is updating according to the transform update
     n=50
     b=0
     while b<2:
@@ -209,38 +245,25 @@ class ToolWatchdogSelfTestTest(ScriptedLoadableModuleTest):
             tools[b].GetMatrixTransformFromParent(toParent)
             toParent.SetElement(0 ,3, a)
             tools[b].SetMatrixTransformFromParent(toParent)
+            
             if a>=n/2:
                 if a==(n/2+10):
-                    self.delayDisplay('Tool_'+str(b)+' should be up to date until watchdog time',6000/speed)
+                    self.delayDisplay('Tool_'+str(b)+' should be up to date until watchdog time',6000/self.Speed)
+                    if(it[b].background().color().name()!='#2de05a' & it[4].background().color().name()!='#2de05a'):
+                        print("Error not same color ON "+'Tool_'+str(b))
+                    if(it[0].background().color().name()!='#ff0000'):
+                        print("Error not same color OFF "+'Tool_0')
                 self.delayDisplay('Tool_'+str(b)+ ' should be up to date',100)
-        self.delayDisplay('wait...',1100/speed)
-        self.delayDisplay('Tools should be out of date',5000/speed)
-
-    watchdogToolToolsTableWidget = slicer.util.findChildren(widget=watchdogWidget, name='ToolsTableWidget')[0]
-    square = qt.QRect()
-    watchdogToolDeleteToolButton = slicer.util.findChildren(widget=watchdogWidget, name='DeleteToolButton')[0]
-    square.setTopLeft(qt.QPoint(0,0))
-    square.setBottomRight(qt.QPoint(0,0))
-    i=0
-    while i<6:
-        watchdogToolToolsTableWidget.setSelection(square,3)
-        watchdogToolDeleteToolButton.click()
-        i=i+1
-    self.delayDisplay('Deleted',5000/speed)
-
-
-    #this adds a toolbar but is not needed
-    #actionIcon = self.parent.icon
-    #a = qt.QAction(actionIcon, 'Download Sample Data', slicer.util.mainWindow())
-    #a.setToolTip('Go to the SampleData module to download data from the network')
-    #a.connect('triggered()', self.select)
-    #mainWin = slicer.util.mainWindow()
-    #newToolBar = qt.QToolBar
-    #mainWin.addToolBar(newToolBar)
-    #fileMenu = slicer.util.lookupTopLevelWidget('FileMenu')
-    #if fileMenu:
-    #  for action in fileMenu.actions():
-    #    if action.text == 'Save':
-    #      fileMenu.insertAction(action,a)
+        self.delayDisplay('wait...',1100/self.Speed)
+        self.delayDisplay('Tools should be out of date',5000/self.Speed)
+        i=0
+        while i<3:
+            if(it[i].background().color().name()!='#ff0000'):
+                print("Error not same color OFF "+'Tool_'+str(i))
+            i=i+1
     
+    #Delete all items on the list
+    watchdogToolToolsTableWidget.selectAll()
+    watchdogToolDeleteToolButton.click()
+    self.delayDisplay('Deleted',5000/self.Speed)
     self.delayDisplay('Test passed!')
