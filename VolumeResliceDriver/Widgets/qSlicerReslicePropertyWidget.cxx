6772b7223c3c5a4d8a7522e5df5266782f12cbc0
Adam Rankin <adam.rankin@gmail.com>
2015-08-11 16:45:16 -0400

    Enh: In progress: Extracting qSlicerReslicePropertyWidget into a reusable widget

---
+++ VolumeResliceDriver/Widgets/qSlicerReslicePropertyWidget.cxx
@@ -0,0 +1,316 @@
+// Qt includes
+#include <QButtonGroup>
+
+#include "qSlicerReslicePropertyWidget.h"
+#include "ui_qSlicerReslicePropertyWidget.h"
+
+// MRMLWidgets includes
+#include <qMRMLSliceWidget.h>
+#include <qMRMLSliceControllerWidget.h>
+#include <qMRMLThreeDWidget.h>
+
+#include "qMRMLViewControllerBar_p.h"
+#include <qMRMLViewControllerBar.h>
+
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QToolButton>
+
+// MRML includes
+#include "vtkSmartPointer.h"
+#include "vtkMRMLSliceNode.h"
+#include "vtkMRMLViewNode.h"
+
+// MRMLLogic includes
+#include "vtkMRMLLayoutLogic.h"
+
+#include "vtkSlicerVolumeResliceDriverLogic.h"
+
+// VTK includes
+#include <vtkSmartPointer.h>
+#include <vtkMatrix4x4.h>
+#include <vtkImageData.h>
+
+#include "ctkPopupWidget.h"
+
+#include "qMRMLColors.h"
+
+
+class qSlicerReslicePropertyWidgetPrivate;
+class vtkMRMLNode;
+class vtkObject;
+
+
+//------------------------------------------------------------------------------
+/// \ingroup Slicer_QtModules_OpenIGTLinkIF
+class qSlicerReslicePropertyWidgetPrivate
+  : public qMRMLViewControllerBarPrivate, public Ui_qSlicerReslicePropertyWidget
+{
+  Q_DECLARE_PUBLIC(qSlicerReslicePropertyWidget);
+  
+public:
+  typedef qMRMLViewControllerBarPrivate Superclass;
+
+  qSlicerReslicePropertyWidgetPrivate(qSlicerReslicePropertyWidget& object);
+  virtual ~qSlicerReslicePropertyWidgetPrivate() ;
+
+  virtual void init();
+  void SetDriverNodeSelection( const char* nodeID );
+  void SetModeSelection( int mode );
+  
+  QButtonGroup methodButtonGroup;
+  QButtonGroup orientationButtonGroup;
+
+  vtkMRMLScene * scene;
+  vtkMRMLSliceNode * sliceNode;
+  vtkMRMLNode  * driverNode;
+
+protected:
+  virtual void setupPopupUi();
+};
+
+
+
+qSlicerReslicePropertyWidgetPrivate
+::qSlicerReslicePropertyWidgetPrivate( qSlicerReslicePropertyWidget& object )
+  : Superclass(object)
+{
+  this->scene = NULL;
+  this->sliceNode = NULL;
+  this->driverNode = NULL;
+}
+
+
+
+qSlicerReslicePropertyWidgetPrivate
+::~qSlicerReslicePropertyWidgetPrivate()
+{
+}
+
+
+
+void qSlicerReslicePropertyWidgetPrivate
+::setupPopupUi()
+{
+  //Q_Q(qSlicerReslicePropertyWidget);
+
+  this->Superclass::setupPopupUi();
+  this->PopupWidget->setAlignment(Qt::AlignBottom | Qt::AlignLeft);
+  this->Ui_qSlicerReslicePropertyWidget::setupUi(this->PopupWidget);
+
+}
+
+
+void qSlicerReslicePropertyWidgetPrivate
+::init()
+{
+  //Q_Q(qSlicerReslicePropertyWidget);
+  
+  this->Superclass::init();
+  this->ViewLabel->setText(qSlicerReslicePropertyWidget::tr("1"));
+  this->BarLayout->addStretch(1);
+  this->setColor(qMRMLColors::threeDViewBlue());
+}
+
+
+void qSlicerReslicePropertyWidgetPrivate
+::SetDriverNodeSelection( const char* nodeID )
+{
+  this->driverNodeSelector->setCurrentNodeID( nodeID );
+}
+
+
+void qSlicerReslicePropertyWidgetPrivate
+::SetModeSelection( int mode )
+{
+  this->modeComboBox->setCurrentIndex( mode );
+}
+
+
+qSlicerReslicePropertyWidget
+::qSlicerReslicePropertyWidget( vtkSlicerVolumeResliceDriverLogic* logic, QWidget *_parent )
+  : Superclass( new qSlicerReslicePropertyWidgetPrivate( *this ), _parent )
+{
+  Q_D(qSlicerReslicePropertyWidget);
+  d->init();
+  this->Logic = logic;
+  qvtkConnect( this->Logic, vtkCommand::ModifiedEvent, this, SLOT( onLogicModified() ) );
+}
+
+
+qSlicerReslicePropertyWidget
+::~qSlicerReslicePropertyWidget()
+{
+  if ( this->Logic != NULL )
+    {
+    this->Logic = NULL;
+    }
+}
+
+
+void qSlicerReslicePropertyWidget
+::showAdvanced( int show )
+{
+  Q_D(qSlicerReslicePropertyWidget);
+  
+  if ( show )
+  {
+    d->rotationSlider->show();
+    d->rotationLabel->show();
+    d->flipCheckBox->show();
+  }
+  else
+  {
+    d->rotationSlider->hide();
+    d->rotationLabel->hide();
+    d->flipCheckBox->hide();
+  }
+}
+
+
+void qSlicerReslicePropertyWidget
+::setSliceViewName( const QString& newSliceViewName )
+{
+  Q_D(qSlicerReslicePropertyWidget);
+  
+  d->ViewLabel->setText(newSliceViewName);
+}
+
+
+void qSlicerReslicePropertyWidget
+::setSliceViewColor( const QColor& newSliceViewColor )
+{
+  Q_D(qSlicerReslicePropertyWidget);
+  
+  d->setColor( newSliceViewColor );
+}
+
+
+
+void qSlicerReslicePropertyWidget
+::setMRMLSliceNode( vtkMRMLSliceNode* newSliceNode )
+{
+  Q_D(qSlicerReslicePropertyWidget);
+  
+  if ( newSliceNode == NULL )
+    {
+    return;
+    }
+  
+  QObject::disconnect(d->driverNodeSelector, SIGNAL(currentNodeChanged(vtkMRMLNode*)), this, SLOT(setDriverNode(vtkMRMLNode*)));
+  QObject::disconnect(d->modeComboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(onModeChanged(int)));
+  QObject::disconnect(d->rotationSlider, SIGNAL(valueChanged(double)), this, SLOT(onRotationChanged(double)));
+  QObject::disconnect(d->flipCheckBox, SIGNAL(stateChanged(int)), this, SLOT(onFlipChanged(int)));
+  
+  d->sliceNode = newSliceNode;
+  
+  if ( newSliceNode->GetScene() )
+    {
+    this->setMRMLScene( newSliceNode->GetScene() );
+    }
+  
+  QObject::connect(d->driverNodeSelector, SIGNAL(currentNodeChanged(vtkMRMLNode*)), this, SLOT(setDriverNode(vtkMRMLNode*)));
+  QObject::connect(d->modeComboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(onModeChanged(int)));
+  QObject::connect(d->rotationSlider, SIGNAL(valueChanged(double)), this, SLOT(onRotationChanged(double)));
+  QObject::connect(d->flipCheckBox, SIGNAL(stateChanged(int)), this, SLOT(onFlipChanged(int)));
+}
+
+
+void qSlicerReslicePropertyWidget
+::setMRMLScene( vtkMRMLScene* newScene )
+{
+  Q_D(qSlicerReslicePropertyWidget);
+
+  
+  if (d->scene != newScene)
+    {
+    d->scene = newScene;
+    if (d->driverNodeSelector)
+      {
+      d->driverNodeSelector->setMRMLScene(newScene);
+      }
+    }
+}
+
+
+void qSlicerReslicePropertyWidget
+::setDriverNode( vtkMRMLNode* newNode )
+{
+  Q_D(qSlicerReslicePropertyWidget);
+  
+  if ( d->sliceNode == NULL )
+    {
+    return;
+    }
+  
+  if ( newNode == NULL )
+    {
+    this->Logic->SetDriverForSlice( "", d->sliceNode );
+    }
+  else
+    {
+    this->Logic->SetDriverForSlice( newNode->GetID(), d->sliceNode );
+    }
+}
+
+
+void qSlicerReslicePropertyWidget
+::onModeChanged(int m)
+{
+  Q_D(qSlicerReslicePropertyWidget);
+
+  this->Logic->SetModeForSlice( m, d->sliceNode );
+
+}
+
+
+
+void qSlicerReslicePropertyWidget
+::onRotationChanged(double r)
+{
+  Q_D(qSlicerReslicePropertyWidget);
+
+  this->Logic->SetRotationForSlice( r, d->sliceNode );
+}
+
+
+
+void qSlicerReslicePropertyWidget
+::onFlipChanged(int f)
+{
+  Q_D(qSlicerReslicePropertyWidget);
+
+  this->Logic->SetFlipForSlice( f, d->sliceNode );
+}
+
+
+
+void qSlicerReslicePropertyWidget
+::onLogicModified()
+{
+  Q_D(qSlicerReslicePropertyWidget);
+  
+  if ( d->sliceNode == NULL )
+    {
+    d->SetDriverNodeSelection( NULL );
+    return;
+    }
+  
+  const char* driverCC = d->sliceNode->GetAttribute( VOLUMERESLICEDRIVER_DRIVER_ATTRIBUTE );
+  d->SetDriverNodeSelection( driverCC );
+  
+  const char* modeCC = d->sliceNode->GetAttribute( VOLUMERESLICEDRIVER_MODE_ATTRIBUTE );
+  if ( modeCC == NULL )
+    {
+    d->SetModeSelection( this->Logic->MODE_NONE );
+    }
+  else
+    {
+    std::stringstream modeSS( modeCC );
+    int mode = this->Logic->MODE_NONE;
+    modeSS >> mode;
+    d->SetModeSelection( mode );
+    }
+
+}
---
+++ VolumeResliceDriver/Widgets/qSlicerReslicePropertyWidget.h
@@ -0,0 +1,86 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) 2011 Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+#ifndef __qSlicerReslicePropertyWidget_h
+#define __qSlicerReslicePropertyWidget_h
+
+// Qt includes
+//#include <QGroupBox>
+#include <QWidget>
+
+// CTK includes
+#include <ctkVTKObject.h>
+
+#include "qMRMLViewControllerBar.h"
+
+// OpenIGTLinkIF GUI includes
+#include "qSlicerVolumeResliceDriverModuleWidgetsExport.h"
+
+class qSlicerReslicePropertyWidgetPrivate;
+class vtkSlicerVolumeResliceDriverLogic;
+class vtkMRMLScene;
+class vtkMRMLNode;
+class vtkMRMLSliceNode;
+class vtkObject;
+
+
+/// \ingroup Slicer_QtModules_OpenIGTLinkIF
+class Q_SLICER_MODULE_VOLUMERESLICEDRIVER_WIDGETS_EXPORT qSlicerReslicePropertyWidget : public qMRMLViewControllerBar
+{
+  Q_OBJECT
+  QVTK_OBJECT
+  
+public:
+  
+  typedef qMRMLViewControllerBar Superclass;
+  
+  qSlicerReslicePropertyWidget( vtkSlicerVolumeResliceDriverLogic* logic, QWidget *parent = 0);
+  virtual ~qSlicerReslicePropertyWidget();
+
+  void showAdvanced( int show );
+  
+  
+public slots:
+  
+  void setSliceViewName(const QString& newSliceViewName);
+  void setSliceViewColor(const QColor& newSliceViewColor);
+  void setMRMLSliceNode(vtkMRMLSliceNode* newSliceNode);
+
+  void setMRMLScene(vtkMRMLScene * newScene);
+  
+protected slots:
+  
+  void setDriverNode(vtkMRMLNode * newNode);
+  void onModeChanged(int);
+  void onRotationChanged(double);
+  void onFlipChanged(int);
+  void onLogicModified();
+  
+  
+private:
+  
+  Q_DECLARE_PRIVATE(qSlicerReslicePropertyWidget);
+  Q_DISABLE_COPY(qSlicerReslicePropertyWidget);
+  
+  vtkSlicerVolumeResliceDriverLogic* Logic;
+};
+
+#endif
+
