e321e16307f5af6a3833850da665c3d2da961a13
RebeccaHisey <rebecca.hisey@gmail.com>
2017-06-14 15:32:37 -0400

    made ultrasound layout a child of procedure layout to allow other widgets to fill the available space on the procedure panel

--- Guidelet/GuideletLib/UltraSound.py
+++ Guidelet/GuideletLib/UltraSound.py
@@ -40,8 +40,12 @@ class UltraSound(object):
     collapsibleButton.text = "Ultrasound"
     parentWidget.addWidget(collapsibleButton)
 
-    ultrasoundLayout = qt.QFormLayout(collapsibleButton)
-    #ultrasoundLayout.setContentsMargins(12,4,4,4)
+    procedureLayout = qt.QVBoxLayout(collapsibleButton)
+    procedureLayout.setContentsMargins(12, 4, 4, 12)
+    procedureLayout.setSpacing(4)
+
+    ultrasoundLayout = qt.QFormLayout()
+    ultrasoundLayout.setContentsMargins(12,4,4,4)
     ultrasoundLayout.setSpacing(4)
 
     self.startStopRecordingButton = qt.QPushButton("Start Recording")
@@ -105,7 +109,9 @@ class UltraSound(object):
       brightnessContrastSliderBox.addWidget(self.brightnessSliderWidget)
       ultrasoundLayout.addRow(brightnessContrastSliderBox)
 
-    return collapsibleButton, ultrasoundLayout
+    procedureLayout.addLayout(ultrasoundLayout)
+
+    return collapsibleButton, ultrasoundLayout, procedureLayout
 
   def setupScene(self):
     logging.info("UltraSound.setupScene")
