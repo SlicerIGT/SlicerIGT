258d197eb3daebec77c3196923316c64f5719ea1
Csaba Pinter <csaba.pinter@queensu.ca>
2019-03-22 14:27:48 -0400

    ENH: Add node references in the registration modules
    This is related to the recent changes in Slicer, SlicerExecutionModel, and BRAINSTools, the goal of which is to update registration modules to have the output transforms reference the inputs (images/fiducial lists/models, fixed and moving separately).

--- FiducialsToModelRegistration/FiducialsToModelRegistration.py
+++ FiducialsToModelRegistration/FiducialsToModelRegistration.py
@@ -218,6 +218,8 @@ class FiducialsToModelRegistrationLogic(ScriptedLoadableModuleLogic):
     icpTransform.Update()
 
     outputTransform.SetMatrixTransformToParent( icpTransform.GetMatrix() )
+    outputTransform.AddNodeReferenceID(slicer.vtkMRMLTransformNode.GetMovingNodeReferenceRole(), inputFiducials.GetID())
+    outputTransform.AddNodeReferenceID(slicer.vtkMRMLTransformNode.GetFixedNodeReferenceRole(), inputModel.GetID())
 
     return True
 
@@ -241,7 +243,7 @@ class FiducialsToModelRegistrationLogic(ScriptedLoadableModuleLogic):
       transformedPoint = [0, 0, 0, 1]
       #transform.GetTransformToParent().TransformVector( originalPoint, transformedPoint )
       originalPoint.append(1)
-      transform.GetTransformToParent().MultiplyPoint( originalPoint, transformedPoint )      
+      transform.GetTransformToParent().MultiplyPoint( originalPoint, transformedPoint )
       #transformedPoints.InsertNextPoint( transformedPoint )
       surfacePoint = [0, 0, 0]
       transformedPoint.pop()
